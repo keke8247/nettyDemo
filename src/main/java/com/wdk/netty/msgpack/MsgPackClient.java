55b2d7291b4f63e811ab2bb993e6f46f1751538d
wdk <wdk@erongdu.com>
2019-04-19 16:14:28 +0800

    netty 集成messagePack 序列化框架 扩展自定义的编码解码器 该版本暂支持半包消息

--- src/main/java/com/wdk/netty/msgpack/MsgPackClient.java
+++ src/main/java/com/wdk/netty/msgpack/MsgPackClient.java
@@ -8,6 +8,8 @@ import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.SocketChannel;
 import io.netty.channel.socket.nio.NioSocketChannel;
+import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
+import io.netty.handler.codec.LengthFieldPrepender;
 
 /**
  * @Description
@@ -29,7 +31,10 @@ public class MsgPackClient {
         bootstrap.handler(new ChannelInitializer<SocketChannel>() {
             @Override
             protected void initChannel(SocketChannel ch) throws Exception {
-                ch.pipeline().addLast("msgDecoder",new MsgpackDecoder())
+                ch.pipeline()
+                        .addLast("frameDecoder",new LengthFieldBasedFrameDecoder(65535,0,2,0,2))
+                        .addLast("msgDecoder",new MsgpackDecoder())
+                        .addLast("frameEncoder",new LengthFieldPrepender(2))
                         .addLast("msgEncoder",new MsgpackEncoder())
                         .addLast("clientHandler",new MsgpackClientHandler());
             }
